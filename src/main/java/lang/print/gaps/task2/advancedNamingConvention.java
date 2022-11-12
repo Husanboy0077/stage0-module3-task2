b8c9125c5d19a7912210a691076cfc41d0b10aa9
Husanboy <olimboyevhusanboy@gmail.com>
2022-11-12 11:03:01 +0500

    play game

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -1,16 +1,18 @@
 package lang.print.gaps.task2;
 
 public class  advancedNamingConvention {
-    private static final int adult_age = 18;
+    private static final int ADULT_AGE = 18;
 
     private int AGE;
     private int phoneNumber;
 
     void CAllToFriend() {
         //some code
+        int a=10;
     }
 
     void callByNumber(int Number) {
         //some code
+        final int HELLO_WORLD = 11;
     }
 }
